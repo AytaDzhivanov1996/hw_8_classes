d0026e7f20de8e607b98152d88299d7489b441bb
AytaDzhivanov1996 <kurt.alive.1994@gmail.com>
2022-12-16 16:04:46 +0300

    сделано

---
+++ hw8_classes_task1.py
@@ -0,0 +1,19 @@
+import os
+
+
+class ChangeDir:
+    def __init__(self, dir_name):
+        self.dir_name = dir_name
+
+    def __enter__(self):
+        return os.chdir(self.dir_name)
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        os.chdir('../')
+
+
+with ChangeDir('dir1'):
+    print(os.listdir())
+
+with ChangeDir('dir2'):
+    print(os.listdir())
